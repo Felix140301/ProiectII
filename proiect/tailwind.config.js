531c5d7c30636406614d2bfcf27d4ba666b3c64d
Felix140301 <orgafelixcosmin@gmail.com>
2024-03-23 22:45:30 +0200

    Front

---
+++ proiect/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      fontFamily: {
+
+        BarlowSemiCondensed:['Barlow Semi Condensed',' sans-serif'],
+        Fraunces:['Fraunces',' serif']
+       
+      }
+    },
+  },
+  plugins: [],
+}
+
